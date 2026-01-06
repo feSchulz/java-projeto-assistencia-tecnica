82a95bdcf7ab220b4ec64aebbe38b95b0b5a7948
Felipe Correia Schulz <zarph1994@gmail.com>
2026-01-05 21:49:37 -0300

    frontend
    adicionando logos icones, header e navbar;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+//components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// Hooks
+import { useAuth } from "./hooks/useAuth";
+
+//Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Navbar/>       
+      </BrowserRouter>
     </div>
   );
 }
